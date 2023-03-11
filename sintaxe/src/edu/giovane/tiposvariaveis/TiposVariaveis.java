04bd09fe69b0a3d351dc7a3e86ec5e495c77771f
giovanesouza <developergiovanesouza@gmail.com>
2023-03-11 18:22:49 -0300

    Exemplos de variáveis/constante - aula: tipos de variáveis

---
+++ sintaxe/src/edu/giovane/tiposvariaveis/TiposVariaveis.java
@@ -0,0 +1,25 @@
+package edu.giovane.tiposvariaveis;
+
+public class TiposVariaveis {
+	
+	public static void main(String[] args) {
+		
+		// variáveis
+		String meuNome = "GIOVANE SOUZA";
+		short ano = 2023;
+		boolean estudandoJava = true;
+		
+		System.out.println("Meu nome: " + meuNome + " - Estudando Java? " + estudandoJava);
+		
+		
+		// constante - final + tipoVariável + NOME_MAIÚSCULO = valor;
+		final double VALOR_DE_PI = 3.14;
+		
+		System.out.println("Valor de PI: " + VALOR_DE_PI);
+
+		
+	}
+	
+
+}
+
