b9dd023de850f076aec311ed1fbc5093ee89677f
giovanesouza <developergiovanesouza@gmail.com>
2023-03-12 14:44:48 -0300

    Exemplos sobre comentários e documentação - aula javadoc

---
+++ sintaxe/src/edu/giovane/javadoc/Comentarios.java
@@ -0,0 +1,32 @@
+package edu.giovane.javadoc;
+
+public class Comentarios {
+	public static void main(String[] args) {
+
+		// One Line
+		// Olá, eu sou um comentário em uma única linha
+		
+		
+		// Multi Line: UM asteríscos após a primeira barra => /*
+		/* Olá,
+		 * Eu sou um comentario
+		 * que posso ser mais detalhadod
+		 * quando necessário
+		 */
+		
+		
+		
+	}
+	
+	//Documentation: UM asteríscos após a primeira barra => /**
+	/** 
+	 * Estas duas estrelinhas acima
+	 * é para identificar que você
+	 * pretende elaborar um comentário
+	 * a nível de documentação.
+	 * Que incrível !!!
+	 */
+	public void metodo() {
+		
+	}
+}
