3e4687366badc4fd2590507e9e3f30ce0469de9b
giovanesouza <developergiovanesouza@gmail.com>
2023-03-14 23:33:29 -0300

    Códigos desenvolvidos ao longo das aulas sobre Estruturas de Repetição e Arrays em Java

---
+++ loops-e-arrays/src/edu/giovane/exercicios/arrays/OrdemInversa.java
@@ -0,0 +1,28 @@
+package edu.giovane.exercicios.arrays;
+
+/*
+Crie um vetor de 6 números inteiros e mostre-os na ordem inversa.
+*/
+public class OrdemInversa {
+
+    public static void main(String[] args) {
+
+        int[] vetor = {-5, -6, 15, 50, 8, 4};
+
+        //System.out.println(vetor.length);
+
+        System.out.print("Vetor - ordem crescente: ");
+        int count = 0;
+        while (count < (vetor.length)) {
+            System.out.print(vetor[count] + " ");
+            count++;
+        }
+
+        System.out.print("\nVetor - ordem decrescente: ");
+        for (int i = (vetor.length - 1); i >= 0; i--) {
+            System.out.print(vetor[i] + " ");
+        }
+
+
+    }
+}
